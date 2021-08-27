0cbf5ed06669c6ececc06ce70d611d61fc927104
eunbi99 <silverbi99@naver.com>
2021-08-27 15:59:54 +0900

    HTTP 요청 메시지와 HTTP 응답

---
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,27 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1(){
+        ModelAndView mav = new ModelAndView("response/hello").addObject("data","hello!");
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model){
+        model.addAttribute("data","hello!");
+        return "response/hello"; //@Controller이면서 String으로 반환하면 뷰의 논리적 이름이 된다!
+    }
+
+    @RequestMapping("/response/hello") //controller 경로와 뷰의 논리적 이름과 같으면 void로 가능( 권장장 X
+    public void responseViewV3(Model model){
+        model.addAttribute("data","hello!");
+    }
+}
